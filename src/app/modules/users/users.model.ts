37704416c656b8e7e5257abbfe6ac3d0a7c4b010
ashik.emon5.im@gmail.com <ashik.emon5.im@gmail.com>
2023-06-02 03:03:09 +0600

    husky

---
+++ src/app/modules/users/users.model.ts
@@ -0,0 +1,32 @@
+import { Model, Schema, model } from 'mongoose'
+import { IUser } from './users.interface'
+
+// Create a new Model type that knows about IUserMethods...
+type UserModel = Model<IUser, object>
+
+// 2. Create a Schema corresponding to the document interface.
+const userSchema = new Schema<IUser>(
+  {
+    id: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    role: {
+      type: String,
+      unique: true,
+    },
+    password: {
+      type: String,
+      unique: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+// 3. Create a Model.
+const User = model<IUser, UserModel>('User', userSchema)
+
+export default User
